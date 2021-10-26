14893baa0968816239c589dc8a205a6edd39f8d0
igor7x7 <80541299+igor7x7@users.noreply.github.com>
2021-10-26 18:21:31 -0300

    finalização front end

--- scripts/script-form.js
+++ scripts/script-form.js
@@ -1,8 +1,22 @@
-var form = document.getElementById("container-form");
-let i = 1;
-form.addEventListener("click", function proximo(event) {
-  
-  form.array.forEach(element => {
-    
-  });
+var button = document.getElementById("button-form");
+var contador = document.getElementById("contador");
+var section = document.querySelectorAll(
+  "section#form-1, section#form-2, section#form-3, section#form-4, section#form-5, section#form-6"
+);
+var i = 0;
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (i < 5) {
+    section[i].style.display = "none";
+    i = i + 1;
+    section[i].style.display = "block";
+    console.log(i);
+  }
+  if (i === 5) {
+    button.textContent = "Concluir";
+    button.addEventListener("click", function () {
+      console.log("enviado");
+    });
+  }
+  contador.textContent = i+1 + "/6";
 });
